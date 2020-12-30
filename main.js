890f0dc9662c38b10334bc0cf356438e16c51d6b
junpyonoh <junepyoo@naver.com>
2020-12-31 06:36:18 +0900

    JS : navbar menu toggle+project UI tweak

--- main.js
+++ main.js
@@ -22,13 +22,22 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
+
+
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', ()=> {
+    
+    navbarMenu.classList.toggle('open');
+});
+
 
 // Handle click on "contact me" button on home
 
 homeBtn = document.querySelector('.home__contact');
-homeBtn.addEventListener('click', ()=>{
+homeBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
